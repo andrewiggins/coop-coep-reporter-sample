dc3d7029a87faa21bb957ab8a6931caef52fc4af
Andre Wiggins <andwi@microsoft.com>
2022-02-21 11:42:55 -0800

    Reduce nesting one level

--- src/website1/server.js
+++ src/website1/server.js
@@ -40,48 +40,51 @@ const documentSecurityHeaders = (nonce) => ({
 	"Referrer-Policy": "strict-origin-when-cross-origin", // Prevent leaking path/query params to other sites
 });
 
-polka()
-	.use(
-		sirv(website1Root("public"), {
-			dev: true,
-			setHeaders(res) {
-				for (let name of Object.keys(commonSecurityHeaders)) {
-					res.setHeader(name, commonSecurityHeaders[name]);
-				}
-			},
-		})
-	)
-	.get("/", async (req, res) => {
-		// Recompile template and integrity on every request to support easy development.
-		// In production, compute these once
+const app = polka();
 
-		const template = await readFile(templatePath, "utf-8");
-		const render = compile(template);
-
-		const nonce = crypto.randomUUID();
-		const alg = "sha256";
-		const scriptIntegrity = crypto
-			.createHash(alg)
-			.update(await readFile(scriptPath))
-			.digest()
-			.toString("base64url");
-
-		res.writeHead(200, "OK", {
-			"Content-Type": "text/html",
-			...commonSecurityHeaders,
-			...documentSecurityHeaders(nonce),
-		});
-
-		res.end(
-			render({
-				nonce: req.query["fail-nonce"] ? "bad-nonce" : nonce,
-				scriptIntegrity: req.query["fail-integrity"]
-					? `${alg}-bad-integrity`
-					: `${alg}-${scriptIntegrity}`,
-			})
-		);
+app.use(
+	sirv(website1Root("public"), {
+		dev: true,
+		setHeaders(res) {
+			for (let name of Object.keys(commonSecurityHeaders)) {
+				res.setHeader(name, commonSecurityHeaders[name]);
+			}
+		},
 	})
-	.listen(8080, (err) => {
-		if (err) throw err;
-		console.log(`> Running on localhost:8080`);
+);
+
+app.get("/", async (req, res) => {
+	// Recompile template and integrity on every request to support easy development.
+	// In production, compute these once
+
+	const template = await readFile(templatePath, "utf-8");
+	const render = compile(template);
+
+	const nonce = crypto.randomUUID();
+	const alg = "sha256";
+	const scriptIntegrity = crypto
+		.createHash(alg)
+		.update(await readFile(scriptPath))
+		.digest()
+		.toString("base64url");
+
+	res.writeHead(200, "OK", {
+		"Content-Type": "text/html",
+		...commonSecurityHeaders,
+		...documentSecurityHeaders(nonce),
 	});
+
+	res.end(
+		render({
+			nonce: req.query["fail-nonce"] ? "bad-nonce" : nonce,
+			scriptIntegrity: req.query["fail-integrity"]
+				? `${alg}-bad-integrity`
+				: `${alg}-${scriptIntegrity}`,
+		})
+	);
+});
+
+app.listen(8080, (err) => {
+	if (err) throw err;
+	console.log(`> Running on localhost:8080`);
+});
